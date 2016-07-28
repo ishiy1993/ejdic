f7ef305224f1b1b56b6dcb4481576713b86fccba
ishiy <y.ishihara.1993@gmail.com>
2016-07-28 19:21:08 +0900

    create base part

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.IO
+
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    as <- getArgs
+    case as of
+      [] -> hPutStrLn stderr helpMsg
+      (a:_) -> search a
+
+helpMsg :: String
+helpMsg = "Need one argument"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,20 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Lens
+import Data.Text.Lens
+import Data.ByteString.Lens
+import Network.Wreq
+
+search :: String -> IO ()
+search w = do
+    let w' = w ^. packed
+    let opt = defaults & params .~ [("Dic","EJdict"),("Word",w'),("Scope","HEADWORD"),("Match","STARTWITH"),("Merge","AND"),("Prof","XHTML"),("PageSize","20"),("PageIndex","0")]
+    res <- getWith opt searchUrl
+    putStrLn $ res ^. responseBody . unpackedChars
+
+searchUrl :: String
+searchUrl = "http://public.dejizo.jp/NetDicV09.asmx/SearchDicItemLite"
+
+getUrl :: String
+getUrl = "http://public.dejizo.jp/NetDicV09.asmx/GetDicItemLite"

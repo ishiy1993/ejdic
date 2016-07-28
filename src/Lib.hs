f1e0ab557837fd174e8f13665edfc3147d205c2a
ishiy <y.ishihara.1993@gmail.com>
2016-07-29 01:00:30 +0900

    ver 0.1

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,7 +21,19 @@ search w = do
         nTitle = n & _nameLocalName .~ "Title"
         ws = doc ^.. root . el n ./ el nTitleList ./ el nDicItemTitle ./ el nTitle ./ el "span" . text
         is = doc ^.. root . el n ./ el nTitleList ./ el nDicItemTitle ./ el nItemID . text
-    print $ zip ws is
+        ls =  zip ws is
+    let id = (lookup w' ls)^._Just
+    let gOpt = defaults & params .~ [("Dic","EJdict"),("Item",id),("Loc",""),("Prof","XHTML")]
+    res2 <- getWith gOpt getUrl
+    let doc2 = parseLBS_ def $ res2 ^. responseBody
+        n2 = doc2 ^. root . name
+        nHead = n2 & _nameLocalName .~ "Head"
+        nBody = n2 & _nameLocalName .~ "Body"
+        h = head $ doc2 ^.. root . el n2 ./ el nHead ./ el "div" ./ el "span" . text
+        b = head $ doc2 ^.. root . el n2 ./ el nBody ./ el "div" ./ el "div" .text
+    putStrLn $ h ^. unpacked
+    putStrLn $ b ^. unpacked
+
 
 searchUrl :: String
 searchUrl = "http://public.dejizo.jp/NetDicV09.asmx/SearchDicItemLite"
